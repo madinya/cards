6e4b18daf1cb01080457f5a5ae57324f07e373e5
Roberto Madinyá <roberto.madinya@ioet.com>
2022-04-18 12:13:08 -0500

    initial commit cards application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: '@madinyini',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '@madinyini-montañero',
+      content: 'El cotopaxi que subí',
+    },
+    {
+      title: 'Richard carapaz',
+      imageUrl: 'assets/biking.jpeg',
+      username: '@madinyini-ciclista',
+      content: 'El rechard carapaz',
+    },    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: '@madinyini',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '@madinyini-montañero',
+      content: 'El cotopaxi que subí',
+    }
+  ];
 }
